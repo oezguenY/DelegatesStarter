3b07471a5c0ad675c601cafe244f03f118c74a3d
Ozgun Yildiz <ozgun.yildiz@hotmail.de>
2022-04-10 21:56:47 +0200

    Initial Commit

---
+++ DelegatesStarter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DelegatesStarter
+//
+//  Created by Özgün Yildiz on 10.04.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
